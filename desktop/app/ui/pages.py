5170f6520584076798c1c1bac9385750d4c103f7
bhonemyinthtoo999-pixel <bhonemyinthtoo999@gmail.com>
2026-07-25 00:38:23 +0630

    Use wide settings and channel mapping workspaces

--- desktop/app/ui/pages.py
+++ desktop/app/ui/pages.py
@@ -3,9 +3,9 @@ from .page_dashboard_responsive import DashboardPage
 from .page_device_safe import DevicePage
 from .page_diagnostics import DiagnosticsPage
 from .page_joystick_polished import JoystickPage
-from .page_mapping_responsive import MappingPage
+from .page_mapping_product import MappingPage
 from .page_profiles_responsive import ProfilesPage
-from .page_settings_themed import SettingsPage
+from .page_settings_product import SettingsPage
 
 __all__ = [
     "CalibrationPage", "DashboardPage", "DevicePage", "DiagnosticsPage",
